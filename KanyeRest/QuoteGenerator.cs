a3372daa32b4182377cc7661b97204533fb4b849
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-09-21 10:37:34 -0700

    Add project files.

---
+++ KanyeRest/QuoteGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeRest
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            string kanyeURL = "https://api.kanye.rest";
+
+            string kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            string kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"----------------------");
+            Console.WriteLine($"Kane:  '{kanyeQuote}'");
+            Console.WriteLine($"----------------------");
+        }
+
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            string ronResponse = client.GetStringAsync(ronURL).Result;
+
+            string ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"--------------------");
+            Console.WriteLine($"Ron:  {ronQuote}");
+            Console.WriteLine($"--------------------");
+        }
+    }
+}
